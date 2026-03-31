b1e482ef2af7e160c0e27e2c2d91671e95077744
Harmehel Singh <harmehel@mac.lan>
2026-03-31 14:31:31 -0700

    remove old deleted files

--- PauseButtonListener.java
+++
@@ -1,8 +0,0 @@
-import java.awt.event.*;
-
-class PauseButtonListener implements ActionListener{
-	public void actionPerformed(ActionEvent e){
-		GrapplePlatformer.PAUSE = !GrapplePlatformer.PAUSE;
-		GrapplePlatformer.graphicsPanel.requestFocusInWindow();
-	}
-}
